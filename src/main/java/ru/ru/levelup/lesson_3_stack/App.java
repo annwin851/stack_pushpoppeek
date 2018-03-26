006bb42b624c43a6853ed48c8fe72be1ffabc3c8
annwin851 <annawin85@yahoo.de>
2018-03-26 18:37:47 +0300

    init commit

---
+++ src/main/java/ru/ru/levelup/lesson_3_stack/App.java
@@ -0,0 +1,23 @@
+package ru.ru.levelup.lesson_3_stack;
+
+import java.util.Stack;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        Stack nums = new Stack();
+        nums.push(15);
+        nums.push(8);
+
+        System.out.println(nums.peek());
+
+        nums.push(10);
+
+        System.out.println(nums.pop());
+
+
+
+    }
+}
